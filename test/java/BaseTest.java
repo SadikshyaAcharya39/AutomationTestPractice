691426f66750ff6639fe51c67d9ae68d1f8c58b9
SadikshyaAcharya39 <acharya.sadikshya33@gmail.com>
2025-03-19 22:01:24 +0545

    Updated

--- test/java/BaseTest.java
+++ test/java/BaseTest.java
@@ -2,10 +2,12 @@ import net.bytebuddy.pool.TypePool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pageobject.BasePage;
@@ -22,6 +24,7 @@ public class BaseTest {
     private WebDriver driver;
     private Properties properties;
     private BasePage basePage;
+    private WebDriverWait wait;
 
     @BeforeEach
     public void setUp(){
@@ -34,8 +37,11 @@ public class BaseTest {
         }
 
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://testautomationpractice.blogspot.com/");
+
+         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         // Send driver
         basePage = new BasePage(driver);
@@ -43,12 +49,12 @@ public class BaseTest {
 
     }
 
-//    @AfterEach
-//    public void tearDown(){
-//     if(driver != null){
-//         driver.quit();
-//     }
-//    }
+    @AfterEach
+    public void tearDown(){
+     if(driver != null){
+         driver.quit();
+     }
+    }
 
     @Test
     public void verifyingForm(){
@@ -73,9 +79,75 @@ public class BaseTest {
         List<WebElement> checkBoxes = driver.findElements(basePage.checkBoxLocator);
         checkBoxes.get(5).click();
 
-        WebElement dropDownELement = driver.findElement(basePage.selectCountryLocator);
-        Select dropDown = new Select(dropDownELement);
-        dropDown.selectByValue("Nepal");
+//        WebElement dropDownELement = driver.findElement(basePage.selectCountryLocator);
+//        Select dropDown = new Select(dropDownELement);
+//        dropDown.selectByValue("Nepal");
+
+
+       WebElement dropDownElement =  driver.findElement(basePage.selectCountryLocator);
+       Select selectCountry = new Select(dropDownElement);
+       selectCountry.selectByVisibleText("Canada");
+
+       WebElement scrollDown = driver.findElement(basePage.selectColourLocator);
+//       scrollDown.sendKeys(Keys.PAGE_DOWN);
+       Select selectColour = new Select(scrollDown);
+       selectColour.selectByVisibleText("Blue");
+
+
+       WebElement dropDownAnimal = driver.findElement(basePage.selectAnimalLocator);
+       dropDownAnimal.sendKeys(Keys.PAGE_DOWN);
+       Select selectAnimal = new Select(dropDownAnimal);
+       selectAnimal.selectByVisibleText("Rabbit");
+
+
+//       WebElement dropDownAnimal = driver.findElement(basePage.selectAnimalLocator);
+//       dropDownAnimal.sendKeys(Keys.PAGE_DOWN);
+//       Select selectAnimal = new Select(dropDownAnimal);
+//       selectAnimal.selectByValue("Rabbit");
+
+
+
+        // Option 1: Data Picker1
+
+//       WebElement datePicker1 = wait.until(ExpectedConditions.elementToBeClickable(basePage.datePicker1Locator0));
+//       datePicker1.click();
+//       datePicker1.sendKeys("03/20/2025");
+
+       // Option 2: Data Picker1
+
+        WebElement datePicker1 = wait.until(ExpectedConditions.visibilityOfElementLocated(basePage.datePicker1Locator0));
+        datePicker1.click();
+        WebElement datePicker10 = wait.until(ExpectedConditions.elementToBeClickable(basePage.datePicker1Locator1));
+        datePicker10.click();
+
+
+        // Option 1: Date Picker 2
+
+        driver.findElement(basePage.datePicker2InputFieldLocator).click();
+        WebElement datePickerTableLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(basePage.datePickerTableLocator));
+        WebElement monthLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(basePage.datePicker2MonthLocator));
+        Select selectMonth = new Select(monthLocator);
+        selectMonth.selectByValue("3");
+        driver.findElement(basePage.datePicker2DateLocator).click();
+
+
+        // Option 1: Date Picker 3
+
+//        WebElement dateManually = driver.findElement(basePage.datePicker3Manually);
+//        dateManually.click();
+//        dateManually.sendKeys("03/20/2025");
+
+
+        // Option 2: Date Picker 3
+
+         WebElement datePicker3table = driver.findElement(basePage.datePicker3Manually);
+         datePicker3table.clear();
+         datePicker3table.sendKeys("03/20/2025");
+
+         WebElement datePicker4Table = driver.findElement(basePage.datePicker4Manually);
+         datePicker4Table.clear();
+         datePicker4Table.sendKeys("03/22/2025");
+
 
 
 
--- test/java/pageobject/BasePage.java
+++ test/java/pageobject/BasePage.java
@@ -25,7 +25,22 @@ public class BasePage
     public By checkBoxLocator = By.xpath("//input[@class='form-check-input' and @type='checkbox']");
 
     public By selectCountryLocator = By.xpath("//select[@class='form-control' and @id='country']");
+    public By selectColourLocator = By.xpath("//select[@class='form-control' and @id='colors']");
+    public By selectAnimalLocator = By.xpath("//select[@class='form-control' and @id='animals']");
 
+    public By datePicker1Locator0 = By.xpath("//input[@id='datepicker']");
+    public By datePicker1Locator1 = By.xpath("//a[@class='ui-state-default' and @data-date='28']");
+
+    public By datePicker2InputFieldLocator = By.xpath("//input[@id='txtDate']");
+    public By datePickerTableLocator = By.xpath("//table[@class='ui-datepicker-calendar']");
+    public By datePicker2MonthLocator = By.xpath("//select[@class='ui-datepicker-month' and @data-handler='selectMonth']");
+    public By datePicker2DateLocator = By.xpath("//a[@class='ui-state-default' and @data-date='22']");
+
+
+    public By datePicker3Manually = By.xpath("//input[@id='start-date']");
+//    public By datePicker3TableLocator1 = By.xpath("//");
+
+    public By datePicker4Manually = By.xpath("//input[@id='end-date']");
 
 
 }
